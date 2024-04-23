c33ea8ceed296d49a89fe319cca81c5a8dc0f888
kubko <hladikjakubis@gmail.com>
2024-04-23 18:17:34 +0200

    fml

--- src/app/api/users/stripe/route.ts
+++ src/app/api/users/stripe/route.ts
@@ -1,9 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import {Stripe} from "stripe";
+import { createClient } from "@/src/lib/supabase/admin";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!,{
     apiVersion: "2023-10-16",
     typescript: true
 });
+const supabase = createClient()
 let data3 = {};
 export async function POST(req:NextRequest){
     const data  = await req.json()
@@ -13,6 +15,7 @@ export async function POST(req:NextRequest){
         phone: data.phone,
         address: data.address
     })
+    supabase.auth.admin.updateUserById(data.id, {user_metadata: {stripe_id: data2.id}})
     data3 = data2
     return NextResponse.json({message: "success"})
 }
