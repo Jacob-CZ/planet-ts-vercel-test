f7857d95f82a914df7396e61af0bbf66e913291e
kubko <hladikjakubis@gmail.com>
2024-04-23 18:08:55 +0200

    testign stripe

--- src/app/api/users/stripe/route.ts
+++ src/app/api/users/stripe/route.ts
@@ -1,10 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
-let user = {}
+import Stripe from "stripe";
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!,{
+    apiVersion: "2023-10-16",
+    typescript: true
+});
+let data3 = {};
 export async function POST(req:NextRequest){
     const data  = await req.json()
-    user = data
+    const data2 = await stripe.customers.create({
+        email: data.email,
+        name: data.name,
+        phone: data.phone,
+        address: data.address
+    })
+    data3 = data2
     return NextResponse.json({message: "success"})
 }
 export function GET(req:NextRequest){
-    return NextResponse.json(user)
+    return NextResponse.json(data3)
 }
