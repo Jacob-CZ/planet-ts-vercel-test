269385856337ba2a6d2b52437083f889dbf51948
kubko <hladikjakubis@gmail.com>
2024-04-26 21:14:21 +0200

    fml

--- src/app/api/pptx_convert/route.ts
+++ src/app/api/pptx_convert/route.ts
@@ -2,19 +2,5 @@ import JSZip from "jszip";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req:NextRequest){
-    const body = await req.formData();
-    const file  = body.get("file");
-    if(!file){
-        return NextResponse.json({error: "No file provided"}, {status: 400});
-    }
-    try{
-        const json  = await pptxToJSON(file as Blob);
-        console.log(json);
-    }
-    catch(error){
-        console.error(error);
-        return NextResponse.json({error: "Failed to convert PPTX to JSON"}, {status: 500});
-    }
-
-    return NextResponse.json({success: true}, {status: 200});
+    return NextResponse.json({success: false}, {status: 404});
 } 
